8629f7b690eb0978d0c6d7be1e13f783d1fa5a15
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-20 08:32:19 +0800

    refactor: Move placeholder data out

--- src/App.svelte
+++ src/App.svelte
@@ -4,70 +4,9 @@
   import Header from './lib/Header.svelte';
   import WikiItem from './lib/WikiItem.svelte';
   import type { RandomPageResponse } from './lib/Wikipedia/types';
+  import { placeholderResponse } from './lib/data';
 
-  let stored: RandomPageResponse[] = [
-    {
-      type: 'standard',
-      title: 'Sant Pere de les Puelles',
-      displaytitle:
-        '<span class="mw-page-title-main">Sant Pere de les Puelles</span>',
-      namespace: {
-        id: 0,
-        text: '',
-      },
-      wikibase_item: 'Q11693387',
-      titles: {
-        canonical: 'Sant_Pere_de_les_Puelles',
-        normalized: 'Sant Pere de les Puelles',
-        display:
-          '<span class="mw-page-title-main">Sant Pere de les Puelles</span>',
-      },
-      pageid: 41200727,
-      thumbnail: {
-        source:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Barcelona_-_Esgl%C3%A9sia_de_Sant_Pere_de_Puelles.jpg/320px-Barcelona_-_Esgl%C3%A9sia_de_Sant_Pere_de_Puelles.jpg',
-        width: 320,
-        height: 240,
-      },
-      originalimage: {
-        source:
-          'https://upload.wikimedia.org/wikipedia/commons/4/43/Barcelona_-_Esgl%C3%A9sia_de_Sant_Pere_de_Puelles.jpg',
-        width: 3072,
-        height: 2304,
-      },
-      lang: 'en',
-      dir: 'ltr',
-      revision: '1267260523',
-      tid: '016325a6-ca76-11ef-835a-5cb3f3ae8d5f',
-      timestamp: '2025-01-04T08:29:25Z',
-      description: 'Benedictine monastery in Barcelona, Catalonia, Spain',
-      description_source: 'local',
-      coordinates: {
-        lat: 41.389661,
-        lon: 2.178761,
-      },
-      content_urls: {
-        desktop: {
-          page: 'https://en.wikipedia.org/wiki/Sant_Pere_de_les_Puelles',
-          revisions:
-            'https://en.wikipedia.org/wiki/Sant_Pere_de_les_Puelles?action=history',
-          edit: 'https://en.wikipedia.org/wiki/Sant_Pere_de_les_Puelles?action=edit',
-          talk: 'https://en.wikipedia.org/wiki/Talk:Sant_Pere_de_les_Puelles',
-        },
-        mobile: {
-          page: 'https://en.m.wikipedia.org/wiki/Sant_Pere_de_les_Puelles',
-          revisions:
-            'https://en.m.wikipedia.org/wiki/Special:History/Sant_Pere_de_les_Puelles',
-          edit: 'https://en.m.wikipedia.org/wiki/Sant_Pere_de_les_Puelles?action=edit',
-          talk: 'https://en.m.wikipedia.org/wiki/Talk:Sant_Pere_de_les_Puelles',
-        },
-      },
-      extract:
-        'Sant Pere de les Puel·les is a Benedictine monastery in the Sant Pere, Santa Caterina i la Ribera neighborhood of the Ciutat Vella district of Barcelona, Catalonia, Spain. The 10th-century building, Romanesque/Gothic in style, was declared a Bien de Interés Cultural landmark in 1931.',
-      extract_html:
-        '<p><b>Sant Pere de les Puel·les</b> is a Benedictine monastery in the Sant Pere, Santa Caterina i la Ribera neighborhood of the Ciutat Vella district of Barcelona, Catalonia, Spain. The 10th-century building, Romanesque/Gothic in style, was declared a Bien de Interés Cultural landmark in 1931.</p>',
-    },
-  ];
+  let stored: RandomPageResponse[] = [placeholderResponse];
 
   let container: HTMLElement;
   let lastExecuted: Date;
