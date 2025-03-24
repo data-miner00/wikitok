5d2875cf386eee74bf0838800a1942f483581485
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-24 19:04:38 +0800

    feat: Implemented favorite page

--- src/lib/views/Favorite.svelte
+++ src/lib/views/Favorite.svelte
@@ -1,3 +1,38 @@
-<div class="h-screen bg-gray-600">
-  <h2>Favorite</h2>
-</div>
+<script lang="ts">
+  import WikiItem from '../WikiItem.svelte';
+  import { localStorageFavoriteKey, localStoragePrefix } from '../constants';
+  import { favoriteList } from '../stores';
+  import type { WikiListItem } from '../types';
+
+  let stored = $state<WikiListItem[]>([]);
+
+  favoriteList.subscribe((value) => {
+    console.log('from store for favorites');
+    console.log(value);
+    stored = value;
+  });
+
+  function removeFromFavorite(title: string) {
+    favoriteList.update((value) => {
+      const updatedList = value.filter((x) => x.title !== title);
+      localStorage.setItem(
+        localStoragePrefix + localStorageFavoriteKey,
+        JSON.stringify(updatedList)
+      );
+      return updatedList;
+    });
+  }
+</script>
+
+<main class="h-dvh h-screen snap-y snap-mandatory overflow-y-scroll">
+  {#each stored as page}
+    <WikiItem
+      backgroundUrl={page.thumbnail}
+      wikiUrl={page.url}
+      title={page.title}
+      excerpt={page.extract}
+      onVisit={() => {}}
+      onFavorite={() => removeFromFavorite(page.title)}
+    />
+  {/each}
+</main>
