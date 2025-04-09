9e2f6db777256b68ba9ecf272119f1e579ff3fc2
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-09 22:51:40 +0800

    fix: Link auto open when removing from list

--- src/lib/ListoverItem.svelte
+++ src/lib/ListoverItem.svelte
@@ -9,35 +9,40 @@
 
   let { wikiUrl, imageUrl, title, excerpt, onXClick }: Props = $props();
 
-  function onRemove(event: MouseEvent) {
-    event.stopPropagation();
-
+  function onRemove() {
     if (onXClick) {
       onXClick(wikiUrl);
     }
   }
 </script>
 
-<a href={wikiUrl} target="_blank" rel="noopener noreferrer" class="group">
+<div class="group">
   <article class="flex items-center">
-    <div class="h-[100px] basis-[100px]">
-      <!-- todo: use smaller image -->
-      <img class="h-full w-full object-cover" src={imageUrl} alt={title} />
-    </div>
-    <div
-      class="{onXClick
-        ? 'basis-[calc(100%-150px)]'
-        : 'basis-[calc(100%-100px)]'} pl-4"
+    <a
+      class="flex items-center"
+      href={wikiUrl}
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      <h2 class="text-2xl group-hover:text-gray-300">{title}</h2>
-      <p class="text-sm">{excerpt}</p>
-    </div>
+      <div class="h-[100px] basis-[100px]">
+        <!-- todo: use smaller image -->
+        <img class="h-full w-full object-cover" src={imageUrl} alt={title} />
+      </div>
+      <div
+        class="{onXClick
+          ? 'basis-[calc(100%-150px)]'
+          : 'basis-[calc(100%-100px)]'} pl-4"
+      >
+        <h2 class="text-2xl group-hover:text-gray-300">{title}</h2>
+        <p class="text-sm">{excerpt}</p>
+      </div>
+    </a>
     {#if onXClick}
-      <div class="basis-[50px]" onclick={onRemove}>
+      <div class="ml-auto basis-[50px]" onclick={onRemove}>
         <button class="mx-auto block cursor-pointer">
           <i class="bi bi-x-lg"></i>
         </button>
       </div>
     {/if}
   </article>
-</a>
+</div>
