fb5d017652bdb45e30e7a0282920c9da9410f2a5
Shaun Chong <data-miner00@users.noreply.github.com>
2026-09-17 21:33:06 +0800

    feat: Double tap (click) to favorite

--- src/lib/WikiItem.svelte
+++ src/lib/WikiItem.svelte
@@ -36,6 +36,7 @@
   }
 
   function handleFavorite(event: MouseEvent) {
+    event.preventDefault();
     isFavorite = !isFavorite;
 
     onFavorite(
@@ -67,7 +68,8 @@
 </script>
 
 <article
-  class="relative h-screen w-screen snap-start bg-slate-400 bg-cover bg-center bg-no-repeat bg-blend-darken"
+  class="relative h-screen w-screen touch-manipulation snap-start bg-slate-400 bg-cover bg-center bg-no-repeat bg-blend-darken"
+  ondblclick={handleFavorite}
   style:background-image={`url('${backgroundUrl}')`}
 >
   <div class="absolute bottom-0 left-0 block w-full bg-black/50 p-4 text-white">
